4ab9bb733b48f76732d2f0c1ceb90b89db2c1cbd
mukul-14-tech <mukulvaidya18@gmail.com>
2026-05-04 14:47:12 +0530

    updated dbjs

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,13 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected ✅");
-  } catch (error) {
-    console.error("DB Error:", error);
+  }catch (error) {
+  console.error("DB Error:", error);
+
+  if (process.env.NODE_ENV !== "test") {
     process.exit(1);
   }
+}
 };
 
 module.exports = connectDB;
